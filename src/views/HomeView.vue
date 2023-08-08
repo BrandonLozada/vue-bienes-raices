063acca82e45799e5befecfd884526103f139ca6
Brandon Lozada <brandon_lozada@outlook.com>
2023-08-07 19:35:04 -0600

    Subir cambios del App.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,5 @@
 </script>
 
 <template>
-  <main>
-    <h1 class="text-center text-h3 font-weight-bold my-5">Vue Bienes Raíces</h1>
-  </main>
+  <h1 class="text-center text-h3 font-weight-bold my-5">Vue Bienes Raíces</h1>
 </template>
