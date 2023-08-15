f685de1fa654fc66ebb66b0c3018d05bcf1f508b
BrandonLozada <brandon_lozada@outlook.com>
2023-08-15 00:53:38 -0600

    Corregir ruta de los componentes View

--- src/router/index.js
+++ src/router/index.js
@@ -22,17 +22,17 @@ const router = createRouter({
         {
           path: '/admin/propiedades',
           name: 'admin-propiedades',
-          component: () => import('..views/admin/AdminView.vue')
+          component: () => import('../views/admin/AdminView.vue')
         },
         {
           path: '/admin/nueva',
           name: 'nueva-propiedad',
-          component: () => import('..views/admin/NuevaPropiedadView.vue')
+          component: () => import('../views/admin/NuevaPropiedadView.vue')
         },
         {
           path: '/admin/editar/:id',
           name: 'editar-propiedad',
-          component: () => import('..views/admin/EditarPropiedadView.vue')
+          component: () => import('../views/admin/EditarPropiedadView.vue')
         }
       ]
     }
